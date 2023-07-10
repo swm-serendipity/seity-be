9c4f2d7a119d96d966fb3ddfd3bb81a977626919
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-07-10 18:12:57 +0900

    [feat] add base response entity

---
+++ src/main/java/com/serendipity/seity/common/exception/CustomRestExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.serendipity.seity.common.exception;
+
+import com.serendipity.seity.common.response.BaseResponse;
+import com.serendipity.seity.common.response.BaseResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class CustomRestExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<BaseResponse<BaseResponseStatus>> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex) {
+        // 아예 잘못된 형식으로 request 를 요청할 경우 예외 발생
+        ex.printStackTrace();
+        return BaseResponse.toResponseEntity(new BaseException(BaseResponseStatus.HTTP_MESSAGE_NOT_READABLE));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<BaseResponse<BaseResponseStatus>> handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException ex) {
+        // 필수 request parameter가 없을 때 발생
+        ex.printStackTrace();
+        return BaseResponse.toResponseEntity(new BaseException(BaseResponseStatus.MISSING_REQUEST_PARAMETER));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<BaseResponseStatus>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+
+        // Validation 오류일 때 발생
+        ex.printStackTrace();
+        return BaseResponse.toResponseEntity(new BaseException(BaseResponseStatus.VALIDATION_EXCEPTION));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public final ResponseEntity<BaseResponse<BaseResponseStatus>> handleMaxUploadSizeExceededException(
+            MaxUploadSizeExceededException ex) {
+
+        // 파일 업로드 용량 초과시 발생
+        ex.printStackTrace();
+        return BaseResponse.toResponseEntity(new BaseException(BaseResponseStatus.FILE_MAX_SIZE_EXCEEDED));
+
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public final ResponseEntity<BaseResponse<BaseResponseStatus>> handleBaseException(BaseException exception) {
+
+        // throws BaseException을 처리
+        return BaseResponse.toResponseEntity(exception);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<BaseResponse<BaseResponseStatus>> handleAllExceptions(Exception ex) {
+        // 따로 핸들링하지 않은 기타 모든 예외를 처리
+        ex.printStackTrace();
+
+        return BaseResponse.toResponseEntity(new BaseException(BaseResponseStatus.UNKNOWN_SERVER_EXCEPTION));
+    }
+
+}
---
+++ src/main/java/com/serendipity/seity/common/response/BaseResponse.java
@@ -0,0 +1,62 @@
+package com.serendipity.seity.common.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.serendipity.seity.common.exception.BaseException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+import static com.serendipity.seity.common.response.BaseResponseStatus.SUCCESS;
+
+/**
+ * 컨트롤러에서 공통으로 반환할 response 클래스입니다.
+ * @param <T> 반환할 데이터 타입
+ *
+ * @author Min Ho CHO
+ */
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"responseTime", "isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime responseTime;
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    private final String message;
+
+    private final int code;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // 요청에 성공한 경우
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+        this.responseTime = LocalDateTime.now();
+    }
+
+    // 요청에 실패한 경우
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+        this.responseTime = LocalDateTime.now();
+    }
+
+    public static ResponseEntity<BaseResponse<BaseResponseStatus>> toResponseEntity(BaseException e) {
+        return ResponseEntity
+                .status(e.getStatus().getHttpStatus())
+                .body(new BaseResponse<>(e.getStatus()));
+    }
+}

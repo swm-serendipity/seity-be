fb7481ecc60291c196e7280eabec25a9cc478d55
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-07-10 15:51:36 +0900

    [feat] add member entity

---
+++ src/main/java/com/serendipity/seity/member/MemberPart.java
@@ -0,0 +1,13 @@
+package com.serendipity.seity.member;
+
+/**
+ * member의 부서를 나타내는 enum 클래스입니다.
+ *
+ * @author Min Ho CHO
+ */
+public enum MemberPart {
+
+    FRONT_END,
+    BACK_END,
+    AI
+}
---
+++ src/main/java/com/serendipity/seity/member/MemberRole.java
@@ -0,0 +1,12 @@
+package com.serendipity.seity.member;
+
+/**
+ * member의 직무를 표현하는 enum 클래스입니다.
+ *
+ * @author Min Ho CHO
+ */
+public enum MemberRole {
+
+    GENERAL,
+    ADMIN
+}

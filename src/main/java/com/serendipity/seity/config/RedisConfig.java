139d4da9fc0e060aea28291114f91921f6aaf1cd
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-07-11 15:41:51 +0900

    [feat] add redis config

---
+++ src/main/java/com/serendipity/seity/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.serendipity.seity.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+/**
+ * redis 설정 관련 config 클래스입니다.
+ *
+ * @author Min Ho CHO
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+}

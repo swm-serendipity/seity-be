b55b4557bd5cd7be8aa26991d4f3b5a68a9fe295
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-07-18 23:17:21 +0900

    [setting] add async config

--- src/main/java/com/serendipity/seity/SeityApplication.java
+++ src/main/java/com/serendipity/seity/SeityApplication.java
@@ -3,9 +3,15 @@ package com.serendipity.seity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.serendipity.seity.member"})
+@EnableMongoRepositories(basePackages = {"com.serendipity.seity.prompt"})
+@EnableAsync
 public class SeityApplication {
 
 	public static void main(String[] args) {

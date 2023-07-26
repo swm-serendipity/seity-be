1e1d5fdfd1a7e34501e91ad132f5dc7cb67d9c14
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-07-26 18:15:36 +0900

    [chore] delete unnecessary import

--- src/main/java/com/serendipity/seity/config/SecurityConfig.java
+++ src/main/java/com/serendipity/seity/config/SecurityConfig.java
@@ -13,9 +13,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Spring Security 관련 config 클래스입니다.

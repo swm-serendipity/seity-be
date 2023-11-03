9358b01021de6b566fe53d11d087bf7d56e95c7e
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-11-03 16:09:12 +0900

    [refactor] add page number to prompt history paging

--- src/main/java/com/serendipity/seity/prompt/dto/PromptPagingResponse.java
+++ src/main/java/com/serendipity/seity/prompt/dto/PromptPagingResponse.java
@@ -1,35 +1,24 @@
 package com.serendipity.seity.prompt.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.serendipity.seity.prompt.Prompt;
-import com.serendipity.seity.prompt.Qna;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 /**
- * 프롬프트 히스토리를 조회할 때 사용되는 response 클래스입니다.
+ * 프롬프트를 페이징할 떄 사용되는 response 클래스입니다.
  *
  * @author Min Ho CHO
  */
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class PromptPagingResponse {
 
-    private String id;
-    private String llm;
-    private String firstQuestion;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, timezone = "Asia/Seoul")
-    private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, timezone = "Asia/Seoul")
-    private LocalDateTime lastModifiedAt;
-    private boolean isFavorite;
+    private int totalPages;
+    private int totalPromptNumber;
+    private int currentPageNumber;
+    private List<SinglePromptPagingResponse> prompts;
 
-    public static PromptPagingResponse of(Prompt prompt) {
-        return new PromptPagingResponse(prompt.getId(), prompt.getLlm(), prompt.getQnaList().get(0).getQuestion(),
-                prompt.getCreateTime(), prompt.getLastModifiedTime(), prompt.isFavorite());
-    }
 }

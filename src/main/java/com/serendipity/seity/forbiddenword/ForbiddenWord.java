41f0d80a33fac7d58690a587a1d83de2e12bd5e9
Min-Ho CHO <66549638+chominho96@users.noreply.github.com>
2023-11-15 16:58:38 +0900

    [feat] add forbidden word entity

---
+++ src/main/java/com/serendipity/seity/forbiddenword/ForbiddenWord.java
@@ -0,0 +1,35 @@
+package com.serendipity.seity.forbiddenword;
+
+import com.serendipity.seity.common.BaseTimeEntity;
+import com.serendipity.seity.member.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+/**
+ * 금칙어를 나타내는 클래스입니다.
+ *
+ * @author Min Ho HO
+ */
+@Entity
+@Getter
+public class ForbiddenWord extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    private String value;
+
+    private ForbiddenWord(String userId, String value) {
+        this.userId = userId;
+        this.value = value;
+    }
+
+    public static ForbiddenWord createForbiddenWord(Member member, String value) {
+
+        return new ForbiddenWord(member.getId(), value);
+    }
+}
